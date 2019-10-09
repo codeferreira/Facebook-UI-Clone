b330537119c0a87d6ee2a8dd06a8f25c158ac124
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-09 20:31:32 -0300

    chore:  Split the page into Components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import logo from './assets/logo.svg';
-
-import './App.css';
+import Header from './components/Header';
+import  Post from './components/Post';
 
 function App() {
+  const [posts,] = useState([
+    {
+      id: 1,
+      author: {
+        name: 'José Carlos Ferreira',
+        avatar: 'https://avatars2.githubusercontent.com/u/25153766?s=460&v=4'
+      },
+      date: '04 Jun 2019',
+      content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
+      comments: [
+        {
+          id: 1,
+          author: {
+            name: 'Robson Marques',
+            avatar: 'https://avatars2.githubusercontent.com/u/861751?s=460&v=4'
+          },
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis risus lectus, vel cursus sem egestas sit amet. Donec eu mi tristique, placerat lacus non, sollicitudin lacus."
+        }
+      ],
+    }
+  ])
+
   return (
     <div className="App">
-      <header>
-        <img src={logo} alt="logo"/>
-        <a href="/" className="perfil">Meu Perfil</a>
-      </header>
-
-      <div className="card">
-        <div className="user">  
-          <img className="ownerAvatar" src="https://avatars2.githubusercontent.com/u/25153766?s=460&v=4" alt="avatar"/>
-          <p className="info">
-            <strong>José Carlos Ferreira</strong>
-            <span>04 Jun 2019</span>
-          </p>
-        </div>
-        <p className="post">Pessoal, alguém sabe se a Rocketseat está contratando?</p>
-        <hr/>
-        <div className="commentContainer">
-          <div className="user">  
-            <img className="ownerAvatar" src="https://avatars2.githubusercontent.com/u/25153766?s=460&v=4" alt="avatar"/>
-          </div>
-          <div className="comment">
-            <p className="text">
-              <strong>José Carlos Ferreira</strong>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis risus lectus, vel cursus sem egestas sit amet. Donec eu mi tristique, placerat lacus non, sollicitudin lacus.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Header />
+      {
+        posts.map(post => <Post post={post}/>)
+      }
+      
     </div>
   );
 }
